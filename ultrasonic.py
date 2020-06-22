85431b1f3c14542c27f468b14cc40641fd4f67d2
Armando Román <48103702+AndoRoman@users.noreply.github.com>
2020-06-22 02:50:00 -0400

    Ultrasonic Sensors Updated

--- ultrasonic.py
+++ ultrasonic.py
@@ -39,11 +39,13 @@ def distance():
 
     return distance
 
+
+###TEST
 if __name__ == '__main__':
     try:
         while True:
             dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
+            print ("Distancia = %.1f cm" % dist)
             time.sleep(1)
 
         # Reset by pressing CTRL + C
