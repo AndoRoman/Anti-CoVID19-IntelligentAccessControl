480005ec415525870d6efec44d87048a7df02366
ARMANDO ANTONIO ROMÁN GONZÁLEZ <20160415@ce.pucmm.edu.do>
2020-11-17 02:07:12 -0400

    Agregando nuevas funcionalidades como: QRreader, SOAPClient, ContadorGeneral, Voces nuevas.

--- QRreader.py
+++ QRreader.py
@@ -4,25 +4,27 @@ import cv2
 cap = cv2.VideoCapture(0)
 # initialize the cv2 QRCode detector
 detector = cv2.QRCodeDetector()
+
+
 def ReadQR():
-    while True:
-        _, img = cap.read()
-        # detect and decode
-        data, bbox, _ = detector.detectAndDecode(img)
-        # check if there is a QRCode in the image
-        if bbox is not None:
-            # display the image with lines
-            for i in range(len(bbox)):
-                # draw all lines
-                cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255, 0, 0), thickness=2)
-            if data:
-                return data
+    _, img = cap.read()
+    # detect and decode
+    data, bbox, _ = detector.detectAndDecode(img)
+    # check if there is a QRCode in the image
+    if bbox is not None:
+        # display the image with lines
+        for i in range(len(bbox)):
+            # draw all lines
+            cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i + 1) % len(bbox)][0]), color=(255, 0, 0), thickness=2)
+        if data:
+            return data
+
 
 def __main__():
-
     print("System ON")
     print("[INFO] QR Code Información:", ReadQR())
     print("System Completed!!")
+
 
 if __name__ == '__main__':
     __main__()
--- SOAPClient.py
+++ SOAPClient.py
@@ -1,6 +1,7 @@
 # import zeep
 import linecache
 import datetime
+import Controlador
 
 # CLIENT SOAP
 # settings = zeep.Settings(strict=False, xml_huge_tree=True)
@@ -12,29 +13,48 @@ import datetime
 
 # information Pre-configurate
 ModuloID = linecache.getline("configuration.txt", 1).strip().split("=")[1]
+# ModuloEstatus indica si es normal (0) o Prioritario (1)
 ModuloEstatus = linecache.getline("configuration.txt", 2).strip().split("=")[1]
 IDSucursal = linecache.getline("configuration.txt", 3).strip().split("=")[1]
+print("[INFO] SOAPClient Inicializado...")
 
 
 # Initialization
 def InitializationSystem():
-    response = cliente.service.inicializar(ModuloID, ModuloEstatus, IDSucursal)
+    # response = MaxCapacidad de la sucursal
+    response = cliente.service.inicializar(IDSucursal)
+
     with open("Syslog.txt", "a") as file:
         file.writelines("\n[Initialization : " + str(datetime.datetime.now()) +
                         "]\n{" + str(response) + "\n}END\n")
         file.close()
 
+    with open("SucursalInfo.txt", "w+") as data:
+        data.writelines("MaxCapacidad=" + str(response) + "\nContador=0")
+        data.close()
 
-def UpdateStatus():
-    cliente.service.Status(ModuloID, Contador, GoodTest, BadTest)
+
+def NewTest(Temp, Mask, QR):
+    test = factory.DtoTesting(mascarilla=Mask, temperatura=Temp, fechaRegistro=datetime.datetime.now(),
+                              idModulo=ModuloID, tipoModulo=ModuloEstatus, cedulaPersona=QR)
+    return test
+
+
+# Enviar Nuevo Test
+def UpdateStatus(Temp, Mask, QR):
+    count = cliente.service.agregarTest(NewTest(Temp=Temp, Mask=Mask, QR=QR))  # Retorna nuevo valor de conteo
+
     with open("Syslog.txt", "a") as file:
-        file.writelines("\n[ PUSH TO CLOUD : " + str(datetime.datetime.now()) + "]\n{"
+        file.writelines("\n[ PUSH TO CLOUD : " + str(test.fechaRegistro) + "]\n{"
                         + "ModuloID = " + str(ModuloID)
-                        + "\nPersonas dentro del Local = " + str(Contador)
-                        + "\nPersonas con Acceso PERMITIDO = " + str(GoodTest)
-                        + "\nPersonas con Acceso DENEGADO = " + str(BadTest)
+                        + "\nPersonas dentro del Local = " + str(count[1])
                         + "\n}END\n")
         file.close()
+    return count
+
+
+def ExitPerson():
+    return cliente.service.salidadDePersona(IDSucursal)
 
 
 # Authenticar QR
@@ -42,7 +62,7 @@ def Authentication(QR):
     with open("Syslog.txt", "a") as file:
         file.writelines("\n[ Authenticate QR : " + str(datetime.datetime.now()) + "]\n{}END\n")
         file.close()
-    return cliente.service.autenticarQR(QR)  # True or False
+    return cliente.service.consultarPrioridad(QR, IDSucursal)  # True or False
 
 
 def __main__():
